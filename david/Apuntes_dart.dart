1440109131e0ad308b2719016663ef8a2ac9a309
David Ramos <davidrtornay@hotmail.com>
2024-03-14 15:40:25 +0100

    Actualizacion 15/40/14/03

---
+++ david/Apuntes_dart.dart
@@ -0,0 +1,71 @@
+//Variables
+void main(){
+  var nombre1="David";
+  print("hola"+nombre1);
+  print("hola $nombre1");  
+
+  int empleados=100;
+  double pi=3.1415;
+  var numero=1.0;
+  
+  print("$empleados - $pi - $numero");
+  
+  String nombre="tony";
+  print(nombre);
+  print(nombre[0]);
+  print(nombre[nombre.length -1]);
+
+  bool activado=true;
+  
+  print(activado);
+
+  //Lista dinámica, se puede introducir numeros, letras, caracteres etc
+  List numeros=[1,2,3,4,5];
+  print(numeros);
+
+  //Lista de string
+   List<int> numeros1=[1,2,3,4,5];
+    print(numeros1);
+
+  //Lista de tamaño fijo
+  List masNumeros=List(10);
+  print(masNumeros);
+  masNumeros.add(1);
+
+
+  //Tipo map String seria la etiqueta de tipo string, y dynamic que el valor de cada string puede ser de cualquier tipo
+  String propiedad='soltero';
+  
+  Map<String, dynamic> persona={
+    'nombre':'Calos',
+    'edad':32,
+    'soltero':true
+  };
+  
+  print(persona['nombre']);
+  print(persona[propiedad]);
+  
+  Map<int,String> personas={
+    1:"tony",
+    2:"peter",
+    9:"Strange"
+  };
+  
+  personas.addAll({4:"banner"});
+  print(personas);
+  print(personas[2]);
+}
+
+//Condiciones
+void condiciones(){
+  bool activado=true;
+  activado=!activado;
+  
+  if(activado){
+    print("El motor esta funcionando");
+  }else{
+    print("Esta apagado");
+  }
+}
+
+//Funciones
