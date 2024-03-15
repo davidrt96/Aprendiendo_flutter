2f769045612dd40b497b6f0131a1b4c4ba9018f7
David Ramos <davidrtornay@hotmail.com>
2024-03-15 23:37:46 +0100

    En proceso de la primera app en Flutter

---
+++ david/ejercicios_flutter/hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "1O",
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            Text(
+              "Cliks",
+              style: TextStyle(fontSize: 25),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
